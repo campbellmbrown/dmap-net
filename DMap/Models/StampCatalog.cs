b085e140d16b1f965d6ea1417a83cef785f5c894
Campbell Brown <campbellbrown093@gmail.com>
2026-08-08 13:10:30 +1200

    Add some simple stamps

--- DMap/Models/StampCatalog.cs
+++ DMap/Models/StampCatalog.cs
@@ -11,19 +11,27 @@ public static class StampCatalog
     [
         new StampTemplate
         {
-            Id = "blue-rectangle",
-            DisplayName = "Bordered Blue Rectangle",
-            AssetPath = "avares://DMap/Assets/Stamps/blue-rectangle.png",
+            Id = "circle",
+            DisplayName = "Circle",
+            AssetPath = "avares://DMap/Assets/Stamps/circle.png",
             DefaultWidth = 180,
-            DefaultHeight = 100,
+            DefaultHeight = 180,
+        },
+        new StampTemplate
+        {
+            Id = "square",
+            DisplayName = "Square",
+            AssetPath = "avares://DMap/Assets/Stamps/square.png",
+            DefaultWidth = 180,
+            DefaultHeight = 180,
         },
         new StampTemplate
         {
             Id = "spirit-guardians",
             DisplayName = "Spirit Guardians",
             AssetPath = "avares://DMap/Assets/Stamps/spirit-guardians.png",
-            DefaultWidth = 100,
-            DefaultHeight = 100,
+            DefaultWidth = 200,
+            DefaultHeight = 200,
         },
     ];
 
@@ -32,7 +40,9 @@ public static class StampCatalog
         foreach (var template in Templates)
         {
             if (template.Id == id)
+            {
                 return template;
+            }
         }
 
         return null;
